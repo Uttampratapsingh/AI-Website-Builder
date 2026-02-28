87beb7fa2a02ea8a75e381f884a20d46eacdcfbf
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-02-28 14:23:11 +0530

    Db connection successfully

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 
 dotenv.config();
+
+await connectDB(); // Connect to the database before starting the server
 
 const app = express();
 const PORT = process.env.PORT || 5000;
