f62339635ce25bd192c752c0ddb700349aeaf7d3
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-02-28 15:16:52 +0530

    Integrated login logout routes to main server

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import authRouter from './routes/auth.js';
+import cookieParser from 'cookie-parser';
 
 
 dotenv.config();
@@ -11,12 +13,21 @@ await connectDB(); // Connect to the database before starting the server
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true, // Allow cookies to be sent
+}));
 app.use(express.json());
+app.use(cookieParser());
+
 
 app.get('/', (req, res) => {
   res.send('Hello from the backend!');
 });
+
+
+app.use('/api/auth', authRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ backend/routes/auth.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { googleAuth, logout } from "../controllers/auth.js";
+
+
+
+
+const authRouter = express.Router();
+
+authRouter.post("/google", googleAuth);
+authRouter.post("/logout", logout);
+
+
+
+export default authRouter;
