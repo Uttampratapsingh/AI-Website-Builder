f64e3981d5561d52acf0de03f4d863b420638575
Uttam Pratap Singh <uttampratapsingh@Uttams-MacBook-Air.local>
2026-03-01 22:34:20 +0530

    Integrate the routes with main server

--- backend/routes/website.js
+++ backend/routes/website.js
@@ -1,12 +1,14 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
-import { generateWebsite, getWebsitesById } from "../controllers/website.js";
+import { changeWebsite, generateWebsite, getAllWebsites, getWebsitesById } from "../controllers/website.js";
 
 
 const websiteRouter = express.Router();
 
 websiteRouter.post("/generate",isAuth, generateWebsite);
+websiteRouter.post("/update",isAuth, changeWebsite);
 
 websiteRouter.get("/get-by-id/:id",isAuth, getWebsitesById);
+websiteRouter.get("/get-all",isAuth, getAllWebsites);
 
 export default websiteRouter;
